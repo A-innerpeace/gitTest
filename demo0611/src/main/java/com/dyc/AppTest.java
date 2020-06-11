e235f69919e67c114d6841b66f863d533cd3b878
dyc <1910912381@qq.com>
2020-06-11 10:33:32 +0800

    01

---
+++ demo0611/src/main/java/com/dyc/AppTest.java
@@ -0,0 +1,16 @@
+package com.dyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: dyc
+ * @Time: 2020/6/11 9:37
+ */
+@SpringBootApplication
+public class AppTest {
+    public static void main(String[] args) {
+        SpringApplication.run(AppTest.class, args);
+    }
+}
---
+++ demo0611/src/main/java/com/dyc/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.dyc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description:
+ * @Author: dyc
+ * @Time: 2020/6/11 9:38
+ */
+@Controller
+public class HelloController {
+    @RequestMapping
+    @ResponseBody
+    public String hello() {
+        return "凡是过往，皆为序章";
+    }
+}
